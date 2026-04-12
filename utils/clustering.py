51ba93d6734a56b55acb9138e4c6f29745538b78
Mkrr <mkrajevsky@gmail.com>
2026-04-12 23:46:30 +0200

    full comparison implemented

--- utils/clustering.py
+++ utils/clustering.py
@@ -9,31 +9,7 @@ Each clustbench.dataset object has number of features most important from our st
 import tqdm
 import clustbench
 
-
-
-def cluster_dataset(dataset, algorithm, alg_name):
-    nclusters= dataset.n_clusters[0]
-    data =  clustbench.preprocess_data(dataset.data)
-    mask = dataset.labels[0]!=0
-    X = data[mask]
-    labels = dataset.labels[0]
-    if alg_name == "GaussianMixture":
-        alg = algorithm(n_components=nclusters)
-    else:
-        alg = algorithm(n_clusters=nclusters)
-    fit = alg.fit_predict(X)
-    return {'Y_pred': fit + 1, 'Y_true': labels}
-
-def cluster_datasets(datasets, algorithm, alg_name):
-    results = {}
-    for (battery, dataset) in  tqdm.tqdm(datasets.items(), desc=f"Clustering datasets with {alg_name}"):
-        results[battery] = cluster_dataset(dataset=dataset, algorithm=algorithm, alg_name=alg_name)
-    return results
-
-
-
 def cluster_dataset_(X, nclusters, labels, algorithm, alg_name):
-    
     if alg_name == "GaussianMixture":
         alg = algorithm(n_components=nclusters)
     else:
